16c1076ce4cef5ba6c857843462583c477572af2
lznism <lznism@lznismdeMacBook-Air.local>
2018-04-19 23:42:00 +0800

    搭建路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { CategoryComponent } from './views/category/category.component';
+import { CategoryItemComponent } from './views/category-item/category-item.component';
+import { MenuItemComponent } from './views/menu-item/menu-item.component';
+import { RankDetailComponent } from './views/rank-detail/rank-detail.component';
+import { SearchResultComponent } from './views/search-result/search-result.component';
 
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'category',
+  component: CategoryComponent
+}, {
+  path: 'category/:id',
+  component: CategoryItemComponent
+}, {
+  path: 'recipe/:id',
+  component: MenuItemComponent
+}, {
+  path: 'explore/:type',
+  component: RankDetailComponent
+}, {
+  path: 'search',
+  component: SearchResultComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoryComponent,
+    CategoryItemComponent,
+    MenuItemComponent,
+    RankDetailComponent,
+    SearchResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/rank-detail/rank-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rank-detail',
+  templateUrl: './rank-detail.component.html',
+  styleUrls: ['./rank-detail.component.css']
+})
+export class RankDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
